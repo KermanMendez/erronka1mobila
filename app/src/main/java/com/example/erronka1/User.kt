2ca01ab113417c22539539448607bf512fcc4387
Kerman Mendez <kerman.mendez.ext@mobility.sener>
2025-10-19 21:46:49 +0200

    User data class fixed

--- app/src/main/java/com/example/erronka1/User.kt
+++ app/src/main/java/com/example/erronka1/User.kt
@@ -5,6 +5,8 @@ data class User(
     var surname: String = "",
     var surname2: String = "",
     var birthdate: String = "",
-    var isTrainer: Boolean = false
+    var isTrainer: Boolean = false,
+    // Need to se if it's necessary to initialize workouts to empty list or to be 2 separated objets
+    var workouts: List<Workout> = emptyList()
 )
 
